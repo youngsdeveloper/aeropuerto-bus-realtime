240e1cfd85660223e75dfaf8497b6e1116631b03
Enrique <kiketos1999@gmail.com>
2024-06-18 22:30:31 +0200

    Feature: Modificados horarios

--- components/Timetables.vue
+++ components/Timetables.vue
@@ -1,18 +1,18 @@
 <template>
     
     <div class="timetable">
-        <h2 class="subtitle is-5">Horarios bus Murcia - Aeropuerto - Cartagena</h2>
+        <h2 class="subtitle is-5">Horarios Bus Murcia - Aeropuerto Corvera</h2>
         
         <table class="table is-striped is-bordered is-fullwidth">
             <thead>
             <tr>
                 <th>Murcia</th>
                 <th>Aeropuerto</th>
-                <th>Cartagena</th>
+                <th>Murcia</th>
             </tr>
             </thead>
             <tbody>
-            <tr v-for="(time,index) in timetableMurciaCartagena" v-bind:key="index">
+            <tr v-for="(time,index) in timetableMurciaAeropuerto" v-bind:key="index">
                 <td>
                 {{ time[0] }}
                 </td>
@@ -20,23 +20,23 @@
                 {{ time[1] }}
                 </td>
                 <td>
-                {{ time[2] }}
+                    {{ time[2] }}
                 </td>
             </tr>
             </tbody>
         </table>
 
-        <h2 class="subtitle is-5">Horarios bus Cartagena - Aeropuerto - Murcia</h2>
+        <h2 class="subtitle is-5">Horarios Bus Cartagena - Aeropuerto Corvera</h2>
         <table class="table is-striped is-bordered is-fullwidth">
             <thead>
             <tr>
                 <th>Cartagena</th>
                 <th>Aeropuerto</th>
-                <th>Murcia</th>
+                <th>Cartagena</th>
             </tr>
             </thead>
             <tbody>
-            <tr v-for="(time,index) in timetableCartagenaMurcia" v-bind:key="index">
+            <tr v-for="(time,index) in timetableCartagenaAeropuerto" v-bind:key="index">
                 <td>
                 {{ time[0] }}
                 </td>
@@ -57,16 +57,35 @@ export default {
     data(){
         return{
 
-            timetableMurciaCartagena: [
-                ["8:30","9:00 - 9:10", "9:55"],
-                ["12:30","13:00 - 13:10", "13:55"],
-                ["19:00","19:30 - 19:40", "20:25"]
+            timetableMurciaAeropuerto: [
+                ["7:30","9:00","-"],
+                ["-","10:00", "10:30"],
+                ["10:30","11:00","-"],
+                ["-","13:00","13:30"],
+                ["13:30","14:00","-"],
+                ["-","14:00","14:30"],
+                ["15:00","15:30","-"],
+                ["-","17:00","17:30"],
+                ["18:00","18:30","-"],
+                ["-","20:30","21:00"],
+                ["21:00","21:30","-"],
+                ["-","21:30","22:00"],
+
+
             ],
 
-            timetableCartagenaMurcia: [
-                ["10:30","11:15 - 11:25", "11:55"],
-                ["14:00","14:45 - 14:55", "15:25"],
-                ["20:30","21:15 - 21:25", "21:55"]
+            timetableCartagenaAeropuerto: [
+                ["-", "8:15", "9:00"],
+                ["9:00", "9:45","-"],
+                ["-", "11:00", "11:45"],
+                ["12:15", "13:00", "-"],
+                ["-", "15:30", "16:15"],
+                ["16:15", "17:00", "-"],
+                ["-", "18:30", "19:15"],
+                ["19:15", "20:00", "-"],
+
+
+
             ]
         }
     }
